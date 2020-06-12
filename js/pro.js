b22412d9296368b06114d93611c2682f99dfdde2
rounakjangid <65699044+rounakjangid@users.noreply.github.com>
2020-06-12 10:42:45 +0530

    Update pro.js

--- js/pro.js
+++ js/pro.js
@@ -14,9 +14,26 @@ function(){
 
     xmlhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
-            div1.innerHTML = this.responseText;
+             var users = JSON.parse(this.responseText);
+
+            var output ='';
+            for(var i in users){
+
+            output  += '<ul>'+
+            '<li>ID: '+users.id+'</li>'+
+            '<li>url:'+url+'</li>'+
+            '<li>name:'+users.name[i]+'</li>'+
+            '<li>type:'+users.type+'</li>'+
+            '<li>language:'+users.language+'</li>'+
+            '</ul>';
+            }
+            
+            document.getElementById('container').
+            innerHTML = output;
         
         }
+        
+        
     };
     xmlhttp.open('GET',url,true);
     xmlhttp.send();
